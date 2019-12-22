2816d7156d0f3aef0330f5507a6969ead8d5625e
rogerphysical <55873122+rogerphysical@users.noreply.github.com>
2019-12-22 18:47:50 +0800

    Add files via upload

--- HW5/BFS_05131056.py
+++ HW5/BFS_05131056.py
@@ -25,8 +25,24 @@ class Graph:
                     if self.check(M, N, i) == False:
                          N.append(i)
           return M
-     
+
+     #後進先出
      def DFS(self, s):
+          M = [s]
+          N = self.graph[s]
+          while len(N) != 0:
+               if self.check(M,[],N[-1]) == True:
+                    N = N[0:-1]
+               else:
+                    M.append(N[-1])
+                    N = N[0:-1]
+                    for i in self.graph[M[-1]]:
+                         if self.check(M, [], i) == False:
+                              N.append(i)
+          return M
+     
+     #先進先出
+     def DFS2(self, s):
           M = [s]
           N = []
           L = len(self.graph[s])
@@ -44,7 +60,6 @@ class Graph:
                          if self.check(M, [], i) == False:
                               N.append(i)
           return M
-
 '''
 g = Graph()
 g.addEdge(0, 1)
@@ -60,8 +75,8 @@ g.addEdge(5, 0)
 g.addEdge(6, 4)
 
 print(g.BFS(2))
-#DFS可製成迷宮
 print(g.DFS(2))
+print(g.DFS2(2))
 '''
 
 #參考資料:自己
